7d542753f3238e69026a6c4f739e895b6283d9a1
AleexWalker <aluurs4039@ieselcaminas.org>
2022-04-21 17:58:42 +0200

    BLE funcional con Scan y Stop

--- app/src/main/java/com/example/scanble/PruebaBLE2.kt
+++ app/src/main/java/com/example/scanble/PruebaBLE2.kt
@@ -3,10 +3,8 @@ package com.example.scanble
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
+import android.bluetooth.*
 import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
@@ -16,9 +14,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.ParcelUuid
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -27,7 +23,12 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
 import com.example.scanble.databinding.ActivityPruebaBle2Binding
 
+@SuppressLint("MissingPermission")
 class PruebaBLE2 : AppCompatActivity() {
+
+    /***************************************************
+     * PROPERTIES
+     ***************************************************/
 
     private lateinit var binding: ActivityPruebaBle2Binding
 
@@ -35,18 +36,48 @@ class PruebaBLE2 : AppCompatActivity() {
     private val LOCATION_PERMISSION_REQUEST_CODE = 2
     private val scanResults = mutableListOf<ScanResult>()
 
+    /**val filter = ScanFilter.Builder().setServiceUuid(
+    ParcelUuid.fromString(ENVIRONMENTAL_SERVICE_UUID.toString())
+    ).build()*/
+
     private val scanResultAdapter: ScanResultAdapter by lazy {
-        ScanResultAdapter(scanResults) {
-            // TODO: Implement
+        ScanResultAdapter(scanResults) { result ->
+            // User tapped on a scan result
+            if (isScanning) {
+                stopBleScan()
+            }
+            with(result.device) {
+                Log.w("ScanResultAdapter", "Connecting to $address")
+                connectGatt(this@PruebaBLE2, false, gattCallback)
+            }
         }
     }
 
-    val isLocationPermissionGranted
+    private val isLocationPermissionGranted
         get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
 
-    /**val filter = ScanFilter.Builder().setServiceUuid(
-        ParcelUuid.fromString(ENVIRONMENTAL_SERVICE_UUID.toString())
-    ).build()*/
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+
+    private var isScanning = false
+        set(value) {
+            field = value
+            runOnUiThread { binding.buttonStart.text = if (value) "Stop Scan" else "Start Scan" }
+        }
+
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .build()
+
+    /***************************************************
+     * ACTIVITY FUNCTION OVERRIDES METHODS
+     ***************************************************/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -101,33 +132,9 @@ class PruebaBLE2 : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private fun startBleScan() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
-            requestLocationPermission()
-        } else {
-            scanResults.clear()
-            scanResultAdapter.notifyDataSetChanged()
-            bleScanner.startScan(null, scanSettings, scanCallback)
-            isScanning = true
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun stopBleScan() {
-        bleScanner.stopScan(scanCallback)
-        isScanning = false
-    }
-
-    private var isScanning = false
-        set(value) {
-            field = value
-            runOnUiThread { binding.buttonStart.text = if (value) "Stop Scan" else "Start Scan" }
-        }
-
-    private val scanSettings = ScanSettings.Builder()
-        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-        .build()
+    /***************************************************
+     * CALLBACK BODIES
+     ***************************************************/
 
     private val scanCallback = object : ScanCallback() {
         @SuppressLint("MissingPermission")
@@ -150,13 +157,48 @@ class PruebaBLE2 : AppCompatActivity() {
         }
     }
 
-    private val bluetoothAdapter: BluetoothAdapter by lazy {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
+    private val gattCallback = object : BluetoothGattCallback() {
+        @SuppressLint("MissingPermission")
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            val deviceAddress = gatt.device.address
+
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    Log.w("BluetoothGattCallback", "Successfully connected to $deviceAddress")
+                    Log.w("Conexión Info", gatt.discoverServices().toString())
+                    gatt.printGattTable()
+                    // TODO: Store a reference to BluetoothGatt
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.w("BluetoothGattCallback", "Successfully disconnected from $deviceAddress")
+                    gatt.close()
+                }
+            } else {
+                Log.w("BluetoothGattCallback", "Error $status encountered for $deviceAddress! Disconnecting...")
+                gatt.close()
+            }
+        }
     }
 
-    private val bleScanner by lazy {
-        bluetoothAdapter.bluetoothLeScanner
+    /***************************************************
+     * PRIVATE FUNCTIONS
+     ***************************************************/
+
+    @SuppressLint("MissingPermission")
+    private fun startBleScan() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
+            requestLocationPermission()
+        } else {
+            scanResults.clear()
+            scanResultAdapter.notifyDataSetChanged()
+            bleScanner.startScan(null, scanSettings, scanCallback)
+            isScanning = true
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun stopBleScan() {
+        bleScanner.stopScan(scanCallback)
+        isScanning = false
     }
 
     private fun requestLocationPermission() {
@@ -183,6 +225,21 @@ class PruebaBLE2 : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
     }
 
+    private fun BluetoothGatt.printGattTable() {
+        if (services.isEmpty()) {
+            Log.i("printGattTable", "No service and characteristic available, call discoverServices() first?")
+            return
+        }
+        services.forEach { service ->
+            val characteristicsTable = service.characteristics.joinToString(
+                separator = "\n|--",
+                prefix = "|--"
+            ) { it.uuid.toString() }
+            Log.i("printGattTable", "\nService ${service.uuid}\nCharacteristics:\n$characteristicsTable"
+            )
+        }
+    }
+
     @SuppressLint("MissingPermission")
     private fun promptEnableBluetooth() {
         if (!bluetoothAdapter.isEnabled) {
@@ -204,7 +261,7 @@ class PruebaBLE2 : AppCompatActivity() {
         }
     }
 
-    fun Context.hasPermission(permissionType: String): Boolean {
+    private fun Context.hasPermission(permissionType: String): Boolean {
         return ContextCompat.checkSelfPermission(this, permissionType) ==
                 PackageManager.PERMISSION_GRANTED
     }
